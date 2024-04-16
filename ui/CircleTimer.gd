5c9aadf56da9f7daaef1bd9f403183e83d10874b
myin <minyin142p@gmail.com>
2024-04-16 11:13:30 +0200

    show customer food wait time

--- ui/CircleTimer.gd
+++ ui/CircleTimer.gd
@@ -1,18 +1,25 @@
 class_name CircleTimer
-extends ColorRect
+extends CanvasItem
 
 @export var timer: Timer
 @export var property := "fill"
+@export var hide_on_stop := true
 
 func _ready():
 	material = material.duplicate()
 
 func _process(delta):
-	visible = not timer.is_stopped()
+	if hide_on_stop:
+		visible = not timer.is_stopped()
+	else:
+		_set_value(0.0)
 	
 	if timer.is_stopped():
 		return
 	
 	var p = timer.time_left / timer.wait_time
+	_set_value(p)
+
+func _set_value(p):
 	var mat = material as ShaderMaterial
 	mat.set_shader_parameter(property, p)
