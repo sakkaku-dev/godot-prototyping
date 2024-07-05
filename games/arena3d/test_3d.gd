389c164d51f3f17dfacc0b27c312036557089117
myin <minyin142p@gmail.com>
2024-07-05 18:51:55 +0200

    add moving grass

--- games/arena3d/test_3d.gd
+++ games/arena3d/test_3d.gd
@@ -1,5 +1,8 @@
 extends Node3D
 
+@onready var grass_2 = $Grass2
+@onready var player = $Player
+
 func _ready():
 	InputMapper.override_key_inputs({
 		"move_up": KEY_W,
@@ -11,3 +14,8 @@ func _ready():
 		"run": KEY_SHIFT,
 		"roll": KEY_CTRL,
 	})
+
+func _process(delta):
+	var mesh = grass_2.mesh as Mesh
+	var mat = mesh.surface_get_material(0) as ShaderMaterial
+	mat.set_shader_parameter("character_position", player.global_position)
